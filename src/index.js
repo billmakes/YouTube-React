682153cc5b44a3d48c9107260c4e7f8df34edbc7
Bill Hilbert <hilbertwilliam@gmail.com>
2018-01-02 22:48:01 -0500

    add search, add styles, finish project

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-const API_KEY = 'AIzaSyCLJCm5J-L2t2Sm9mOrrF9ZldXBNukAcVo';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAtPKzj0nqGRGpTvAblPHrOzckDhu_jc9A';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+	}
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
 }
-
-
-// Take this component's generated HTML and put it on the DOM
 
 ReactDOM.render(<App />, document.querySelector('.container'));
